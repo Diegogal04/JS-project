b05e476e6876b5230cd4a8142bcfdc6cee400b4a
Diegogal04 <68531355+Diegogal04@users.noreply.github.com>
2021-07-05 18:31:10 -0500

    terminacion de funcion js para botones

--- main.js
+++ main.js
@@ -2,14 +2,30 @@ const btn = document.getElementsByClassName("btn");
 const slide = document.getElementById("slide");
 
 btn[0].onclick = function() {
-    slide.style.transform = "translateX(0px)"
+    slide.style.transform = "translateX(0px)";
+    for (let i = 0; i < 4; i++) {
+        btn[i].classList.remove("active")
+    }
+    this.classList.add("active")
 }
 btn[1].onclick = function() {
-    slide.style.transform = "translateX(-800px)"
+    slide.style.transform = "translateX(-800px)";
+    for (let i = 0; i < 4; i++) {
+        btn[i].classList.remove("active")
+    }
+    this.classList.add("active")
 }
 btn[2].onclick = function() {
-    slide.style.transform = "translateX(-1600px)"
+    slide.style.transform = "translateX(-1600px)";
+    for (let i = 0; i < 4; i++) {
+        btn[i].classList.remove("active")
+    }
+    this.classList.add("active")
 }
 btn[3].onclick = function() {
-    slide.style.transform = "translateX(-2400px)"
+    slide.style.transform = "translateX(-2400px)";
+    for (let i = 0; i < 4; i++) {
+        btn[i].classList.remove("active")
+    }
+    this.classList.add("active")
 }
